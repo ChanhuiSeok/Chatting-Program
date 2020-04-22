27cb967b9874577dd6df24a4364c37fe2838d896
unknown <chanhuicom@gmail.com>
2020-04-23 00:05:44 +0900

    chatPage 생성 및 material-ui 일부 적용

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import LandingPage from './views/LandingPage/LandingPage';
 import LoginPage from './views/LoginPage/LoginPage';
 import RegisterPage from './views/RegisterPage/RegisterPage';
+import ChatPage from './views/ChatPage/ChatPage';
 import Auth from '../hoc/auth'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={Auth(LandingPage, null)} />
         <Route exact path="/login" component={Auth(LoginPage, false)} />
         <Route exact path="/register" component={Auth(RegisterPage, false)} />
+        <Route exact path="/main" component={Auth(ChatPage, true)} />
       </Switch>
     </div>
   </Router>
